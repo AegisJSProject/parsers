5deefd2c1de090f7a1dc810f5fc7e0d5cbdf9bfd
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-25 08:54:54 -0700

    Add URL parser & fix bad config passing
    ### Added
    - Add `url` parser
    - Add new types supported in `stringify`
    
    ### Fixed
    - Fix infinite recursion in config via update to `@aegisjsproject/sanitizer`

--- parsers.js
+++ parsers.js
@@ -7,4 +7,5 @@ export { svg } from './svg.js';
 export { math } from './math.js';
 export { xml } from './xml.js';
 export { json } from './json.js';
+export { url } from './url.js';
 export { createPolicy } from './trust.js';
