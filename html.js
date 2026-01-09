4f68768a8336d62ebd049060b34f130f1f388b99
Chris Zuber <shgysk8zer0@gmail.com>
2026-01-09 09:18:17 -0800

    Refactor HTML parser and add dev server config
    Refactored html.js to use <template> elements for fragment creation and introduced a reusable raw template function. Added http.config.js and index.html for local development using @aegisjsproject/dev-server and @aegisjsproject/http-utils. Updated .npmignore to exclude index.html, removed test/index.html, and updated dependencies in package.json to support the new dev server setup.

--- html.js
+++ html.js
@@ -1,32 +1,32 @@
 import { sanitizer } from '@aegisjsproject/sanitizer/config/base.js';
 import { stringify } from './utils.js';
 
+const raw = (strings, values, mapper = stringify) => String.raw(strings, ...values.map(mapper)).trim();
+
 export function createHTMLParser(config = sanitizer, { mapper = stringify } = {}) {
 	return (strings, ...values) => {
-		const frag = document.createDocumentFragment();
-		const tmp = document.createElement('div');
-		tmp.setHTML(String.raw(strings, ...values.map(mapper)).trim(), { sanitizer:  config });
-		frag.append(...tmp.childNodes);
-		return frag;
+		const tmp = document.createElement('template');
+		tmp.setHTML(raw(strings, values, mapper), { sanitizer:  config });
+
+		return tmp.content;
 	};
 }
 
 export const html = createHTMLParser(sanitizer, { mapper: stringify });
 
 export function htmlUnsafe(strings, ...values) {
-	const frag = document.createDocumentFragment();
-	const tmp = document.createElement('div');
-	tmp.setHTMLUnsafe(String.raw(strings, ...values.map(stringify)).trim());
-	frag.append(...tmp.childNodes);
-	return frag;
+	const tmp = document.createElement('template');
+	tmp.setHTMLUnsafe(raw(strings, values));
+
+	return tmp.content;
 }
 
 export const el = (...args) => html.apply(null, args).firstElementChild;
 
 export function doc(strings, ...values) {
-	return Document.parseHTML(String.raw(strings, ...values.map(stringify)), { sanitizer });
+	return Document.parseHTML(raw(strings, values), { sanitizer });
 }
 
 export function docUnsafe(strings, ...values) {
-	return Document.parseHTMLUnsafe(String.raw(strings, ...values.map(stringify)));
+	return Document.parseHTMLUnsafe(raw(strings, values));
 }
